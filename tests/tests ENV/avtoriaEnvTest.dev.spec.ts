2795580abae4225503c3722c97aba6ed34692a38
OleksiySolomakha <alekseysomik@gmail.com>
2026-03-13 00:01:47 +0200

    added test for check how does ENV works - added small test files for stage and dev - added scripts for run only stage and only dev files

--- tests/tests ENV/avtoriaEnvTest.dev.spec.ts
+++ tests/tests ENV/avtoriaEnvTest.dev.spec.ts
@@ -1,9 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('Check my car dev from stage', async ({ page }) => {
-    await page.goto('');
+test.describe('Check how does ENV works', () => {
 
-    await expect(page.locator('a[href="https://auto.ria.com/uk/car/bmw/5-series/"]')).toHaveText('5 Серія');
-    await page.getByRole('button', { name: ' Пошук '}).click();
-    await expect(page.locator('input[value="BMW"]')).toBeVisible();
-});
+    test('Should not see car from stage on dev', async ({ page }) => {
+        await page.goto('');
+    
+        await expect(page.locator('a[href="https://auto.ria.com/uk/car/bmw/5-series/"]')).toHaveText('5 Серія');
+        await page.getByRole('button', { name: ' Пошук '}).click();
+        await expect(page.locator('input[value="BMW"]')).toBeVisible();
+    });
+
+    test('Check correct card on dev', async({page}) => {
+        await page.goto('');
+    
+        await page.getByRole('button', { name: ' Пошук '}).click();
+        await expect(page.locator('input[value="Alfa Romeo"]')).toBeVisible();
+        await expect(page.locator('input[value="159"]')).toBeVisible();
+    })
+
+})
+
+
